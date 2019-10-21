5ad36825a5c2e21bb656ec0da3da45c3864c736f
Konrad Werys <konradwerys2@gmail.com>
2019-10-21 13:00:02 +0200

    feat: adding mex files

--- cmake/tomato_download.cmake
+++ cmake/tomato_download.cmake
@@ -8,18 +8,20 @@ if (APPLE)
     set(SYSTEM_NAME "osx")
 endif (APPLE)
 
-MESSAGE(STATUS "https://github.com/MRKonrad/tomato/releases/download/v0.4/TomatoLib_v0.4_${SYSTEM_NAME}.zip")
+MESSAGE(STATUS "https:/github.com/MRKonrad/tomato/releases/download/v0.4/TomatoLib_v0.4_${SYSTEM_NAME}.zip")
 
-if (NOT EXISTS ${PROJECT_SOURCE_DIR}//Tomato//TomatoLib.zip)
+if (NOT EXISTS ${PROJECT_SOURCE_DIR}/Tomato/TomatoLib.zip)
     file(DOWNLOAD
-            "https://github.com/MRKonrad/tomato/releases/download/v0.4/TomatoLib_v0.4_${SYSTEM_NAME}.zip"
-            "${PROJECT_SOURCE_DIR}//Tomato//TomatoLib.zip"
+            "https:/github.com/MRKonrad/tomato/releases/download/v0.4/TomatoLib_v0.4_${SYSTEM_NAME}.zip"
+            "${PROJECT_SOURCE_DIR}/Tomato/TomatoLib.zip"
             STATUS status LOG log)
     list(GET status 0 status_code)
     list(GET status 1 status_string)
+
+    execute_process( COMMAND dir "${PROJECT_SOURCE_DIR}/Tomato/"
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/Tomato/)
+    execute_process( COMMAND cmake -E tar xzf "${PROJECT_SOURCE_DIR}/Tomato/TomatoLib.zip"
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/Tomato/)
 endif()
 
-execute_process( COMMAND dir "${PROJECT_SOURCE_DIR}//Tomato//"
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}//Tomato//)
-execute_process( COMMAND cmake -E tar xzf "${PROJECT_SOURCE_DIR}//Tomato//TomatoLib.zip"
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}//Tomato//)
+
