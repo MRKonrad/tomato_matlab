abccbbeb5fd31822e42bd909979cdcade6fd34f9
Konrad Werys <konradwerys2@gmail.com>
2019-10-21 15:55:58 +0200

    feat: adding mex files v9

--- cmake/tomato_download.cmake
+++ cmake/tomato_download.cmake
@@ -10,17 +10,17 @@ endif (APPLE)
 
 MESSAGE(STATUS "https:/github.com/MRKonrad/tomato/releases/download/v0.4/TomatoLib_v0.4_${SYSTEM_NAME}.zip")
 
-if (NOT EXISTS ${PROJECT_SOURCE_DIR}/Tomato/TomatoLib.zip)
+if (NOT EXISTS ${PROJECT_SOURCE_DIR}/Tomato/Tomato.zip)
     file(DOWNLOAD
             "https:/github.com/MRKonrad/tomato/releases/download/v0.4/TomatoLib_v0.4_${SYSTEM_NAME}.zip"
-            "${PROJECT_SOURCE_DIR}/Tomato/TomatoLib.zip"
+            "${PROJECT_SOURCE_DIR}/Tomato/Tomato.zip"
             STATUS status LOG log)
     list(GET status 0 status_code)
     list(GET status 1 status_string)
 
     execute_process( COMMAND dir "${PROJECT_SOURCE_DIR}/Tomato/"
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/Tomato/)
-    execute_process( COMMAND cmake -E tar xzf "${PROJECT_SOURCE_DIR}/Tomato/TomatoLib.zip"
+    execute_process( COMMAND cmake -E tar xzf "${PROJECT_SOURCE_DIR}/Tomato/Tomato.zip"
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/Tomato/)
 endif()
 
