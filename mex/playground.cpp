49af7e64a22933b49f5958bd5e3f4ad826d01996
Konrad Werys <konradwerys2@gmail.com>
2019-10-25 15:47:40 +0200

    build: changing rpath

--- mex/playground.cpp
+++ mex/playground.cpp
@@ -1,4 +1,5 @@
 #include "mex.h"
+#include "OxCalculatorT1Molli.h"
 
 void
 mexFunction (int nlhs, mxArray *plhs[],
@@ -7,4 +8,7 @@ mexFunction (int nlhs, mxArray *plhs[],
     mexPrintf ("Hello, World!\n");
 
     mexPrintf ("I have %d inputs and %d outputs\n", nrhs, nlhs);
+
+    Ox::CalculatorT1Molli<double> calculator;
+    calculator.disp();
 }
