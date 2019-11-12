eaaae8e0b33f8ab1b3a76615a9abfdf1251c85f5
Konrad Werys <konradwerys2@gmail.com>
2019-11-12 12:11:48 +0100

    chore: tempararly adding testData

--- tests/readStructTest.m
+++ tests/readStructTest.m
@@ -1,13 +1,10 @@
 %% test passing a struct should not crash
-
+pwd
 yamlFilePath = 'testData/tomatoConfig_Shmolli_inputFilePaths.yaml';
-mystruct = yaml.ReadYaml(yamlFilePath);
+mystruct = yaml.ReadYaml(yamlFilePath)
 
 if isOctave
     playground_octave_mex(mystruct);
 else
     playground_matlab_mex(mystruct);
 end
-
-% MyTomatoMatlabStruct = TomatoMatlabStruct;
-% playground_matlab_mex(struct(MyTomatoMatlabStruct));
